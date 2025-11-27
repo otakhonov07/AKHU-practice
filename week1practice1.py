b15d08a126e5bf7366398c3cd44cdbcff764a92d
Otabek <otaxonovotabek@dmail.com>
2025-11-27 19:41:36 +0500

    Add these files

---
+++ week1practice1.py
@@ -0,0 +1,7 @@
+name = input("What's your name?")
+major = input("What's your major?")
+hobby = input("What's your hobby?")
+
+print("Your name is:" + name)
+print("Your major is:" + major)
+print("Your hobby is:" + hobby)
---
+++ week1practice2.py
@@ -0,0 +1,6 @@
+number1 = int(input("Enter a number, please."))
+number2 = int(input("Enter the second number, please."))
+
+result = number1 + number2
+
+print("The sum of your numbers is", result)
